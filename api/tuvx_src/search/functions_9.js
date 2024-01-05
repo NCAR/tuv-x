74ca5dea0d03f8c62d9af1a99d0659e203c293b9
mattldawson <mattldawson@users.noreply.github.com>
2024-01-05 19:50:34 +0000

    deploy: a8bb45bc31682ff5fd1e8b8b52dd14d76832c72f

---
+++ api/tuvx_src/search/functions_9.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['solver_5fallocate_327',['solver_allocate',['../namespacetuvx__solver__factory.html#a6fd76a98cd745b7315ee3b6246516a0e',1,'tuvx_solver_factory']]],
+  ['solver_5fbuilder_328',['solver_builder',['../namespacetuvx__solver__factory.html#ac1b65cb8559883b72de5558df316d136',1,'tuvx_solver_factory']]],
+  ['solver_5ftype_5fname_329',['solver_type_name',['../namespacetuvx__solver__factory.html#a8fdee9381268fa69222abe6c3fc914a1',1,'tuvx_solver_factory']]]
+];
