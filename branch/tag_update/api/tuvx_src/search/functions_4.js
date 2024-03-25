12df17def9e402ae16f5b1148f0ba6d2665f4999
mattldawson <mattldawson@users.noreply.github.com>
2024-03-25 19:00:44 +0000

    deploy: 3be5dde88bedc75e7479b3738c412dfa1375e903

---
+++ branch/tag_update/api/tuvx_src/search/functions_4.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['get_5fcopy_5fchar_312',['get_copy_char',['../structtuvx__cross__section__warehouse_1_1cross__section__warehouse__t.html#a9781b4841814847bb5164c47c861c791',1,'tuvx_cross_section_warehouse::cross_section_warehouse_t']]],
+  ['get_5fgrid_5fchar_313',['get_grid_char',['../structtuvx__grid__warehouse_1_1grid__warehouse__t.html#a93a66e41417eadca458ce574fa5d28d4',1,'tuvx_grid_warehouse::grid_warehouse_t']]],
+  ['get_5fiterator_314',['get_iterator',['../structtuvx__radiator__warehouse_1_1radiator__warehouse__t.html#ad9b97c2b0781ddd40606b8c78bdbd814',1,'tuvx_radiator_warehouse::radiator_warehouse_t']]],
+  ['get_5fptr_5fchar_315',['get_ptr_char',['../structtuvx__cross__section__warehouse_1_1cross__section__warehouse__t.html#a7f3ca696a527ffc1aba64f32ce6f6388',1,'tuvx_cross_section_warehouse::cross_section_warehouse_t::get_ptr_char()'],['../structtuvx__grid__warehouse_1_1grid__warehouse__t.html#a17eeff818f6f11d92f93ffedcee8b2fb',1,'tuvx_grid_warehouse::grid_warehouse_t::get_ptr_char()'],['../structtuvx__radiator__warehouse_1_1radiator__warehouse__t.html#a3f84ec17ada6173beae91dd5619d2f82',1,'tuvx_radiator_warehouse::radiator_warehouse_t::get_ptr_char()']]],
+  ['get_5fupdater_316',['get_updater',['../structtuvx__grid__warehouse_1_1grid__warehouse__t.html#a4554f6e81c2d01772f03ece5aa7f8104',1,'tuvx_grid_warehouse::grid_warehouse_t::get_updater()'],['../structtuvx__radiator__warehouse_1_1radiator__warehouse__t.html#acde1b6e0b823a9c3a1bcd0e16a6067d0',1,'tuvx_radiator_warehouse::radiator_warehouse_t::get_updater()']]]
+];
