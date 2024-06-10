a966ecbeb5f4c983e093e70173d371c3b52b2b12
Jiwon Gim <jiwongim@ucar.edu>
2024-06-10 16:00:38 -0600

    single line style

--- test/unit/util/array3d.cpp
+++ test/unit/util/array3d.cpp
@@ -1,7 +1,5 @@
-/* Copyright (C) 2023-2024 National Center for Atmospheric Research
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright (C) 2023-2024 National Center for Atmospheric Research
+// SPDX-License-Identifier: Apache-2.0
 #include <tuvx/util/array3d.hpp>
 
 #include <gtest/gtest.h>
