dd2a520a9ce56d34f0fd15c8ef1716fc7c3173b5
Aditya Dendukuri <adityad@derecho4.hsn.de.hpc.ucar.edu>
2024-06-10 11:34:12 -0600

    removed LAPACKE test; test tridiagonal accuracy for various sizes

--- test/unit/linear_algebra/test_tridiagonal_solver.cpp
+++ test/unit/linear_algebra/test_tridiagonal_solver.cpp
@@ -4,7 +4,6 @@
 #include <ctime>
 #include <gtest/gtest.h>
 #include <limits>
-#include <mkl_lapacke.h>
 #include <tuvx/linear_algebra/linear_algebra.hpp>
 #include <vector>
 
@@ -21,71 +20,38 @@ const double tol_dp = std::numeric_limits<double>::epsilon();
 const float tol_sp = std::numeric_limits<float>::epsilon();
 const int norm_order = 2; // L2 norm for computing error
 
+/// @test Tridiagonal Solver Test for single Precision Floats.
+/// @brief Generate random tridiagonal matrix $A$ and vector $x$,
+/// compute $b=A \cdot x$, and check if solution is reconstructed
+/// accurately using L2 norm (single precision). Check for different
+/// sizes to check consistency
+TEST(TridiagSolveTest, SinglePrecision) {
+  std::size_t sizes[5] = {5, 10, 1000, 100000, 10000000};
+  vecf errors = vecf(5);
+  for (auto &e : errors) {
+    vecf x(size);
+    vecf b(size);
+    trid_matf A(size);
+
+    FillRandom<float>(A);
+    FillRandom<float>(x);
+
+    b = Dot<float>(A, x);
+    vecf x_approx = Solve<float>(A, b);
+
+    e = ComputeError<float>(x, x_approx, norm_order);
+  }
+  for (auto e : errors) {
+    EXPECT_LE(e, tol_sp);
+  }
+}
+
 /// @test Tridiagonal Solver Test for Double Precision Floats.
 /// @brief Generate random tridiagonal matrix $A$ and vector $x$,
 /// compute $b=A \cdot x$, and check if solution is reconstructed
-/// accurately using L2 norm (double precision).
+/// accurately using L2 norm (double precision). Check for different
+/// sizes to check consistency
 TEST(TridiagSolveTest, DoublePrecision) {
-  vecd x(size);
-  vecd b(size);
-  trid_matd A(size);
-
-  // initialize random matrix A and vector
-  FillRandom<double>(A);
-  FillRandom<double>(x);
-  b = Dot<double>(A, x);
-
-  // reconstruct x by tridiagonal solve
-  clock_t start = clock();
-  vecd x_approx = Solve<double>(A, b);
-  clock_t stop = clock();
-
-  // reconstruct x by tridiagonal solve
-  double error = ComputeError<double>(x, x_approx, norm_order);
-
-  std::cout << "TUVX ERROR: " << error << std::endl;
-  std::cout << "TUVX SPEED: " << (stop - start) / (double)CLOCKS_PER_SEC
-            << std::endl;
-  EXPECT_LE(error, tol_dp) << "DOUBLE EPSILON: "
-                           << std::numeric_limits<double>::epsilon()
-                           << std::endl;
-}
-
-TEST(TridiagSolveTest, CompareWithLAPACKE) {
-  vecd x(size);
-  vecd b(size);
-  trid_matd A(size);
-
-  FillRandom<double>(A);
-  FillRandom<double>(x);
-
-  b = Dot<double>(A, x);
-  // vecd x_approx = Solve<double>(A, b);
-
-  clock_t start = clock();
-  LAPACKE_dgtsv(LAPACK_ROW_MAJOR, size, 1, A.lower_diagonal_.data(),
-                A.main_diagonal_.data(), A.upper_diagonal_.data(), b.data(), 1);
-  clock_t stop = clock();
-
-  vecd lapacke_result = vecd(b.data(), b.data() + size);
-
-  double lapacke_error = ComputeError<double>(lapacke_result, x, norm_order);
-  // double tridiag_error = ComputeError<double>(x_approx, x, norm_order);
-
-  std::cout << "LAPACKE ERROR: " << lapacke_error << std::endl;
-  std::cout << "LAPACKE SPEED: " << (stop - start) / (double)CLOCKS_PER_SEC
-            << std::endl;
-  // std::cout << "TUVX-LINALG ERROR: " << tridiag_error << std::endl;
-
-  EXPECT_LE(lapacke_error, tol_dp);
-  // EXPECT_LE(tridiag_error, tol_dp);
-}
-
-/// @test Test for verifying the consistency in approximation error for various
-/// sizes
-/// @brief Test the relative error function for various system sizes and make
-/// sure that it is consistent
-TEST(TridiagSolveTest, ErrorConsistencyDifferentSizes) {
   std::size_t sizes[5] = {5, 10, 1000, 100000, 10000000};
   vecd errors = vecd(5);
   for (auto &e : errors) {
@@ -104,26 +70,4 @@ TEST(TridiagSolveTest, ErrorConsistencyDifferentSizes) {
   for (auto e : errors) {
     EXPECT_LE(e, tol_dp);
   }
-}
-
-/// @test Tridiagonal Solver Test for single Precision Floats.
-/// @brief Generate random tridiagonal matrix $A$ and vector $x$,
-/// compute $b=A \cdot x$, and check if solution is reconstructed
-/// accurately using L2 norm (single precision).
-TEST(TridiagSolveTest, SinglePrecision) {
-  vecf x(size);
-  vecf b(size);
-  trid_matf A(size);
-
-  FillRandom<float>(A);
-  FillRandom<float>(x);
-
-  b = Dot<float>(A, x);
-  vecf x_approx = Solve<float>(A, b);
-
-  float error = ComputeError<float>(x, x_approx, norm_order);
-
-  EXPECT_LE(error, tol_sp) << "FLOAT EPSILON: "
-                           << std::numeric_limits<float>::epsilon()
-                           << std::endl;
 }
