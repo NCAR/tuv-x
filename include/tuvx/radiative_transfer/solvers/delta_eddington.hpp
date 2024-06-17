6ecc1b9fd90a7689f9620d6947ad6967dae6ba4a
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2024-06-17 15:02:56 -0600

    single line style (#94)

--- include/tuvx/radiative_transfer/solvers/delta_eddington.hpp
+++ include/tuvx/radiative_transfer/solvers/delta_eddington.hpp
@@ -1,9 +1,7 @@
-/* Copyright (C) 2023-2024 National Center for Atmospheric Research
- *
- * SPDX-License-Identifier: Apache-2.0
- *
- * Delta-Eddington solver for radiative transfer.
- */
+// Copyright (C) 2023-2024 National Center for Atmospheric Research
+// SPDX-License-Identifier: Apache-2.0
+//
+// Delta-Eddington solver for radiative transfer
 #pragma once
 
 #include <tuvx/grid.hpp>
