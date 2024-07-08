73ac682c24b73e706baedcc21433e4122d2e04a5
Aditya Dendukuri <adityad@derecho1.hsn.de.hpc.ucar.edu>
2024-07-08 08:55:33 -0600

    documentation changes

--- include/tuvx/linear_algebra/linear_algebra.hpp
+++ include/tuvx/linear_algebra/linear_algebra.hpp
@@ -17,49 +17,59 @@ namespace tuvx
     std::vector<T> upper_diagonal_;  // upper diagonal
     std::vector<T> lower_diagonal_;  // lower diagonal
     std::vector<T> main_diagonal_;   // main diagonal
-    /// @brief tridiagonal matrix structure constructor
+    /// @fn tridiagonal matrix structure constructor
+    /// @brief initializes the three internal vectors based on
+    /// size. Main diagonal will be of size $n$, while the upper/lower
+    /// diagonals will be of size $n-1#
     TridiagonalMatrix(std::size_t size);
   };
 
-  /// @brief Fills a vector with uniformly distributed random values.
-  /// @param x Vector to and fill
+  /// @brief Fill a std::vector with random values
+  /// @param x Vector to allocate and fill
   /// @param seed for random number generation
   template<typename T>
   void FillRandom(std::vector<T> &x, const unsigned &seed);
 
   /// @brief Fills a matrix with uniformly distributed random values.
-  /// @param A tridiagonal matrix to allocate and fill
-  /// @param seed for random number generation
-  /// @param make_diagonally_dominant Flag that is set to true if matrix is diagonally dominant; false otherwise
-  ;
+  /// @param A Tridiagonal matrix to allocate and fill
+  /// @param seed For random number generation
+  /// @param make_diagonally_dominant Make the tridiagonal matrix diagonally dominant (diagonal value is greater than sum of
+  /// the corrosponding row)
   template<typename T>
   void FillRandom(TridiagonalMatrix<T> &A, const unsigned &seed, const bool &make_diagonally_dominant = false);
 
-  /// @brief display contents of std::vector
+  /// @fn print vector function
+  /// @brief displays the data stored inside a std::vector
   /// @param x Vector to print
   template<typename T>
   void Print(const std::vector<T> &x);
 
+  /// @fn print matrix function
   /// @brief displays the data stored inside a tridiagonal matrix.
+  /// For now, this function simply calls printvec() on the three diagonals.
   /// @param A tridiagonal matrix to print
   template<typename T>
   void Print(const TridiagonalMatrix<T> &A);
 
+  /// @fn Tridiagonal Linear System Solver
   /// @brief Thomas' algorithm for solving tridiagonal linear system (A x = b).
+  /// store solution in b.
   /// @param A tridiagonal coeffcient matrix
   /// @param b right hand side vector of the tridiagonal system.
   template<typename T>
   void Solve(TridiagonalMatrix<T> &A, std::vector<T> &b);
 
   /// @brief Specialized dot product function for tridiagonal matrices.
-  /// @param A tridiagonal matrix
-  /// @param x vector to multiply the matrix with
+  /// @param A Tridiagonal matrix
+  /// @param x Vector to multiply the matrix with
+  /// @returns Dot product between A and x
   template<typename T>
   std::vector<T> Dot(const TridiagonalMatrix<T> &A, const std::vector<T> &b);
 
-  /// @brief Computes the relative error between two vectors.
-  /// @param x true solution
-  /// @param x_approx approximated solution
+  /// @brief Computes the LP error norm between two vectors. Used for computing
+  /// approximation errors.
+  /// @param x True solution
+  /// @param x_approx Approximated solution
   template<typename T>
   T ComputeError(const std::vector<T> &x, const std::vector<T> &x_approx);
 
