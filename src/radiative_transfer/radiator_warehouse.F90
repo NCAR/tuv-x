0a6f99058eb0c88a8f3e532a3c45e261a253eafd
Jiwon Gim <jiwongim@ucar.edu>
2024-07-30 19:14:02 -0600

    test code

--- src/radiative_transfer/radiator_warehouse.F90
+++ src/radiative_transfer/radiator_warehouse.F90
@@ -254,7 +254,7 @@ contains
     ! write(*,*) "  [jiwon] name", this%radiators_( ndx )%val_%handle_
     do ndx = 1, size( this%radiators_ )
       if( name .eq. this%radiators_( ndx )%val_%handle_ ) then
-        write(*,*) "  [jiwon] name", this%radiators_( ndx )%val_%handle_
+        ! write(*,*) "  [jiwon] name", this%radiators_( ndx )%val_%handle_
         found = .true.
         exit
       endif
