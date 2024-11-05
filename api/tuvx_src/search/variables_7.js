a966e33decfab341eca81f6c64d61a4f0e5badfd
mattldawson <mattldawson@users.noreply.github.com>
2024-11-05 21:35:13 +0000

    deploy: fbe0f8aa73f6630d230c6463b603d6ba64c65dcf

---
+++ api/tuvx_src/search/variables_7.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['max_5ftemperature_5f_0',['max_temperature_',['../structtuvx__temperature__range_1_1temperature__range__t.html#a766c859e9af2fd12499daf283687c69c',1,'tuvx_temperature_range::temperature_range_t']]],
+  ['max_5fwavelength_5f_1',['max_wavelength_',['../structtuvx__temperature__parameterization_1_1temperature__parameterization__t.html#a7476abd9a8f323d4c8404b5a6f8749f9',1,'tuvx_temperature_parameterization::temperature_parameterization_t']]],
+  ['max_5fwavelength_5findex_5f_2',['max_wavelength_index_',['../structtuvx__temperature__parameterization_1_1temperature__parameterization__t.html#a4742b39cf9ced7382ccf00120962b942',1,'tuvx_temperature_parameterization::temperature_parameterization_t']]],
+  ['min_5ftemperature_5f_3',['min_temperature_',['../structtuvx__temperature__range_1_1temperature__range__t.html#a29b4e479c3a63de728d171b05ec06159',1,'tuvx_temperature_range::temperature_range_t']]],
+  ['min_5fwavelength_5f_4',['min_wavelength_',['../structtuvx__temperature__parameterization_1_1temperature__parameterization__t.html#a5b07eff07a616ae39e7d65157c6c6491',1,'tuvx_temperature_parameterization::temperature_parameterization_t']]],
+  ['min_5fwavelength_5findex_5f_5',['min_wavelength_index_',['../structtuvx__temperature__parameterization_1_1temperature__parameterization__t.html#aad4ae22452a28b7956194efc677a878d',1,'tuvx_temperature_parameterization::temperature_parameterization_t']]],
+  ['molecular_5fdiameter_5f_6',['molecular_diameter_',['../structtuvx__quantum__yield__h2so4__mills_1_1quantum__yield__h2so4__mills__t.html#a322e9921d816981391ea15b7fb6e2e54',1,'tuvx_quantum_yield_h2so4_mills::quantum_yield_h2so4_mills_t']]],
+  ['molecular_5fweight_5f_7',['molecular_weight_',['../structtuvx__quantum__yield__h2so4__mills_1_1quantum__yield__h2so4__mills__t.html#aabdd8469764985420beb380924e27e2d',1,'tuvx_quantum_yield_h2so4_mills::quantum_yield_h2so4_mills_t']]],
+  ['musica_5fdk_8',['musica_dk',['../namespacemusica__constants.html#a63fad7bccbda60ae33ec2375a10cb825',1,'musica_constants']]],
+  ['musica_5flk_9',['musica_lk',['../namespacemusica__constants.html#af35077407fe1edac71338680a756be8d',1,'musica_constants']]],
+  ['musica_5frk_10',['musica_rk',['../namespacemusica__constants.html#af52842e4d461b07c0a97f61e76ee5124',1,'musica_constants']]]
+];
---
+++ branch/develop-3-initial-optics/api/tuvx_src/search/all_d.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['radiation_5ffield_5ft_120',['radiation_field_t',['../structtuvx__solver_1_1radiation__field__t.html',1,'tuvx_solver']]],
+  ['radiative_5ftransfer_5ft_121',['radiative_transfer_t',['../structtuvx__radiative__transfer_1_1radiative__transfer__t.html',1,'tuvx_radiative_transfer']]],
+  ['radiator_5faerosol_5ft_122',['radiator_aerosol_t',['../structtuvx__radiator__aerosol_1_1radiator__aerosol__t.html',1,'tuvx_radiator_aerosol']]],
+  ['radiator_5ffrom_5fhost_5ft_123',['radiator_from_host_t',['../structtuvx__radiator__from__host_1_1radiator__from__host__t.html',1,'tuvx_radiator_from_host']]],
+  ['radiator_5ffrom_5fnetcdf_5ffile_5ft_124',['radiator_from_netcdf_file_t',['../structtuvx__radiator__from__netcdf__file_1_1radiator__from__netcdf__file__t.html',1,'tuvx_radiator_from_netcdf_file']]],
+  ['radiator_5fptr_125',['radiator_ptr',['../structtuvx__radiator_1_1radiator__ptr.html',1,'tuvx_radiator']]],
+  ['radiator_5fstate_5ft_126',['radiator_state_t',['../structtuvx__radiator_1_1radiator__state__t.html',1,'tuvx_radiator']]],
+  ['radiator_5ft_127',['radiator_t',['../structtuvx__radiator_1_1radiator__t.html',1,'tuvx_radiator']]],
+  ['radiator_5fupdater_5ft_128',['radiator_updater_t',['../structtuvx__radiator__from__host_1_1radiator__updater__t.html',1,'tuvx_radiator_from_host']]],
+  ['radiator_5fwarehouse_5fptr_129',['radiator_warehouse_ptr',['../structtuvx__radiator__warehouse_1_1radiator__warehouse__ptr.html',1,'tuvx_radiator_warehouse']]],
+  ['radiator_5fwarehouse_5ft_130',['radiator_warehouse_t',['../structtuvx__radiator__warehouse_1_1radiator__warehouse__t.html',1,'tuvx_radiator_warehouse']]],
+  ['ranges_5f_131',['ranges_',['../structtuvx__cross__section__temperature__based_1_1temperature__parameterization__t.html#a841d96ff4f51f380a41991255e2ad426',1,'tuvx_cross_section_temperature_based::temperature_parameterization_t']]],
+  ['refraction_132',['refraction',['../structtuvx__cross__section__o3__tint_1_1cross__section__o3__tint__t.html#aae12db64102a6999b0bd8237b8365c4d',1,'tuvx_cross_section_o3_tint::cross_section_o3_tint_t']]],
+  ['reset_133',['reset',['../structtuvx__radiator__warehouse_1_1warehouse__iterator__t.html#a983acb057491d9d9b62bca194f51a222',1,'tuvx_radiator_warehouse::warehouse_iterator_t']]]
+];
