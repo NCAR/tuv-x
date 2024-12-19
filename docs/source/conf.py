2b03db7b74338fbcddeaaba4559fe807bc7b1618
Montek Thind <mthind@ucar.edu>
2024-12-19 12:06:34 -0800

    release tuvx (#116)
    * release tuvx
    
    * update cmake version
    
    * added comma
    
    * fixed version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'NCAR/UCAR'
 suffix = os.getenv("SWITCHER_SUFFIX", "")
 
 # The full version, including alpha/beta/rc tags
-release = f'v0.10{suffix}'
+release = f'v0.10.1{suffix}'
 
 
 # -- General configuration ---------------------------------------------------
