f4b8a629ebe0472877d89da9a7eb7474c5569ac2
peverwhee <courtneyp@ucar.edu>
2026-02-10 17:00:26 -0700

    cleanup whitespace

--- src/util/assert.F90
+++ src/util/assert.F90
@@ -53,6 +53,7 @@ contains
 
   !> Asserts condition to be true or fails with provided message
   subroutine assert_msg_string( code, condition, error_message )
+
     use musica_string,                 only : string_t
 
     !> Unique code for the assertion
